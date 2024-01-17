9858cefcaafcc84629601e5aa42bccb3558f1000
Ida Sofie Grude <sofie.grude@gmail.com>
2024-01-17 08:21:28 +0100

    endringer

--- src/components/svarside.js
+++ src/components/svarside.js
@@ -1,81 +1,3 @@
-// import React, { useState } from "react";
-// import SvarBox from "./SvarBox";
-// import SpørsmålsComponent from "./SpørsmålComponent";
-// import TidligereSvar from "./TidligereSvar";
-// import API from "../../api";
-
-// import "../index.css";
-
-// function SvarSide() {
-//   const [tidligereSvar, setTidligereSvar] = useState([
-//     {
-//       navn: "Ida",
-//       overskrift: "Tre takk",
-//       innhold: "Takk for sol. Takk for at gelato finnes. Takk for høstfarger.",
-//       id: 1,
-//     },
-//     {
-//       navn: "Ida",
-//       overskrift: "Takk for muligheten",
-//       innhold: "Takk for muligheten t å reisa.",
-//       id: 2,
-//     },
-//     {
-//       navn: "Ida",
-//       overskrift: "Mat",
-//       innhold: "Takk for at kyllingsalaten blei så god.",
-//       id: 3,
-//     },
-//   ]);
-
-//   let [idCounter, setIdCounter] = useState(4);
-
-// //   function Lagrefunksjon(svaret) {
-// //     let svarObject = {
-// //       navn: "Ida",
-// //       overskrift: "Midlertidig overskrift",
-// //       innhold: svaret,
-// //       id: idCounter,
-// //     };
-// //     setIdCounter(idCounter + 1);
-// //     let nySvarliste = [svarObject, ...tidligereSvar];
-// //     setTidligereSvar(nySvarliste);
-// //   }
-
-// //   return (
-// //     <div className="App">
-// //       <SpørsmålsComponent spørsmål="List tre ting du er takknemlig for idag." />
-
-// //       <SvarBox funksjonSomLagrer={Lagrefunksjon} />
-
-// //       <TidligereSvar svarListe={tidligereSvar} />
-// //     </div>
-// //   );
-// // }
-
-// function Lagrefunksjon(svaret) {
-//   let svarObject = {
-//     navn: "Ida",
-//     overskrift: "Midlertidig overskrift",
-//     innhold: svaret,
-//     id: idCounter,
-//   };
-//   setIdCounter(idCounter + 1);
-//   let nySvarliste = [svarObject, ...tidligereSvar];
-//   setTidligereSvar(nySvarliste);
-
-//   // Send POST-forespørsel til Flask API
-//   API.post('/post/svar', svarObject)
-//     .then(response => {
-//       console.log('Svar lagret:', response.data);
-//     })
-//     .catch(error => {
-//       console.error('Feil ved lagring av svar:', error);
-//     });
-// }
-
-// export default SvarSide;
-
 import React, { useState, useEffect } from "react";
 import SvarBox from "./SvarBox";
 import SpørsmålsComponent from "./SpørsmålComponent";
@@ -105,8 +27,8 @@ function SvarSide() {
   function Lagrefunksjon(svaret) {
     let svarObject = {
       navn: "Ida",
-      overskrift: "Midlertidig overskrift",
-      innhold: svaret,
+      overskrift: "Tre ting",
+      svar_innhold: svaret,
       id: idCounter,
     };
     setIdCounter(idCounter + 1);

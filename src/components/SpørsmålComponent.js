bb71f9a99e49fe0374b077fe766a3864c64e15cf
Ida Sofie Grude <sofie.grude@gmail.com>
2024-04-10 10:53:03 +0200

    fikset skrivefeil i spørsmålskomp

--- src/components/SpørsmålComponent.js
+++ src/components/SpørsmålComponent.js
@@ -2,8 +2,8 @@ import React from "react";
 
 // her tas hele propsobjektet inn, det vil si at for hver prop vi sender inn i parent component ligger det en field
 // alternativ kunne man dekomponert props objektet
-function SpørsmålsComponent({ sprøsmål }) {
-  return <section className="SpørsmålsComponent">{sprøsmål}</section>;
+function SpørsmålsComponent({ spørsmål }) {
+  return <section className="SpørsmålsComponent">{spørsmål}</section>;
 }
 
 export default SpørsmålsComponent;
